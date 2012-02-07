02156985c8125196832475cbd295dc87eab39885
James McPherson <jemcphe@fullsail.edu>
2012-02-07 16:51:21 -0600

    Function getGolfBallCount
    Created function to get the golf ball count.  Running into a bit of a
    problem.  My current logic is telling the computer I have 10 balls, yet
    it is beginning with 9.  I do want it to decrement, but i want it to
    start from 10 initially, not 9.

--- mcpherson_james_assignment2.js
+++ mcpherson_james_assignment2.js
@@ -14,14 +14,26 @@ var whereToPlay = function(courseName){
 
 var readyToGolf = function (isRaining, playerCount) {
 	if((isRaining === true) || (playerCount < 4)) {
-		var message = console.log("Luck is not on our side. No golf today.");
-		return message;
+		var noLuck = console.log("Luck is not on our side. No golf today.");
+		return noLuck;
 	} else {
 		if ((isRaining === false) && (playerCount >= 4)) {
-			var message = console.log("We have "+ playerCount + " players, and the weather is perfect");
-			return message;
+			var luckyDay = console.log("We have "+ playerCount + " players, and the weather is perfect");
+			return luckyDay;
 		}
 	}
 };
+
+var getGolfBallCount = function(ballCount) {
+	var ballCount = 10;
+	var currentBallCount = ballCount;
+	while (currentBallCount > 1) {
+		currentBallCount-= 1;
+		var lostBall = currentBallCount - 1;
+		console.log("I had " + ballCount + " golf balls.  Poor play has left me with " + lostBall + " golf balls.");
+	}
+	return ballCount;
+}
 whereToPlay("Thundercreek");
 readyToGolf(false , 4);
+getGolfBallCount();
