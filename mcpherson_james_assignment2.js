83b69bb6151c810c62fd0f7bd60ad5e9f2e9c163
James McPherson <jemcphe@fullsail.edu>
2012-02-07 16:08:23 -0600

    add var function readyToGolf
    This is a Boolean function that takes arguments (isRaining,
    playerCount).

--- mcpherson_james_assignment2.js
+++ mcpherson_james_assignment2.js
@@ -2,7 +2,7 @@ alert("JavaScript works!");
 
 //var courseName = ["Arrowhead", "Thundercreek", "Country Club"];
 var playerCount = 0;
-var isRaining = false;
+//var isRaining = false;
 
 var whereToPlay = function(courseName){
 	if(courseName === "Arrowhead") {
@@ -10,6 +10,18 @@ var whereToPlay = function(courseName){
 	} else {
 		console.log("I hate the " + courseName + " golf course!");
 	}
-}
+};
 
+var readyToGolf = function (isRaining, playerCount) {
+	if((isRaining === true) || (playerCount < 4)) {
+		var message = console.log("Luck is not on our side. No golf today.");
+		return message;
+	} else {
+		if ((isRaining === false) && (playerCount >= 4)) {
+			var message = console.log("We have "+ playerCount + " players, and the weather is perfect");
+			return message;
+		}
+	}
+};
 whereToPlay("Thundercreek");
+readyToGolf(false , 4);
