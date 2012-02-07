2297f357bc765416a5203f1da60dacfd4caf3b4e
James McPherson <jemcphe@fullsail.edu>
2012-02-07 15:53:20 -0600

    Set up HTML/JS Procedure
    Created Procedure to determine which golf course should be played.

--- mcpherson_james_assignment2.js
+++ mcpherson_james_assignment2.js
@@ -1,1 +1,15 @@
 alert("JavaScript works!");
+
+//var courseName = ["Arrowhead", "Thundercreek", "Country Club"];
+var playerCount = 0;
+var isRaining = false;
+
+var whereToPlay = function(courseName){
+	if(courseName === "Arrowhead") {
+		console.log("We are going to play at the ", courseName, " golf course today.");
+	} else {
+		console.log("I hate the " + courseName + " golf course!");
+	}
+}
+
+whereToPlay("Thundercreek");
